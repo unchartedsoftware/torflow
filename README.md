01e00b2ce00abc948ca35368cf7c978417f0acc6
David.Schroh <dschroh@uncharted.software>
2015-07-16 10:04:33 -0400

    license and start of docker

---
+++ README.md
@@ -0,0 +1,19 @@
+# TorFlow
+
+## Building
+
+Install modules:
+
+    npm install
+
+Build:
+
+    cd public
+    gulp
+
+## Running
+
+## Ingest Data
+
+## Using TorFlow
+
--- docker/gulpfile.js
+++ docker/gulpfile.js
@@ -25,29 +25,14 @@
 * SOFTWARE.
 */
 
-var DotLayer = L.CanvasLayer.extend({
+var gulp            = require('gulp');
+var path            = require('path');
+//var docker          = require('gulp-docker');
 
-    _ctx : null,
-    _initialized : false,
+var paths = {
+  tocopy: ['../public', '!client/external/**/*.coffee']
+};
 
-    add : function(latLng) {
-        if (!this._initialized) {
-            return;
-        }
-        var point = this._map.latLngToContainerPoint(latLng);
-        this._ctx.fillRect(point.x,point.y,1,1);
-    },
+gulp.src('../public')
+  .pipe(gulp.dest('./public'));
 
-    render: function() {
-        if (!this._initialized) {
-            var canvas = this.getCanvas();
-            this._ctx = canvas.getContext('2d');
-
-            this._ctx.fillStyle = 'rgba(0, 0, 255, 0.01)';
-            this._ctx.clearRect(0, 0, canvas.width, canvas.height);
-            this._initialized = true;
-        }
-    }
-});
-
-module.exports = DotLayer;
