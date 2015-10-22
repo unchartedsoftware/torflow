67cf158e3d10b46a1b5d1036495e0bc2aadfe5ab
kbirk <birk.kevin@gmail.com>
2015-10-22 14:29:58 -0400

    Country table will now contain all countries for each date, rather than top 50. The number to limit is passed as a query param to the /country endpoint.

--- db/guardclients.js
+++ db/guardclients.js
@@ -1,11 +1,9 @@
 var connectionPool = require('./connection');
 var config = require('../config');
 
-var NUM_COUNTRIES = 50;
-
 var getCountryHistogram = function(date,callback) {
     connectionPool.query(
-        'SELECT * FROM ' + config.db.database + '.guard_clients WHERE date=? GROUP BY cc ORDER BY guardclientcount DESC LIMIT ' + NUM_COUNTRIES,
+        'SELECT * FROM ' + config.db.database + '.guard_clients WHERE date=? GROUP BY cc ORDER BY guardclientcount DESC',
         [date],
         function(err,rows) {
             if (err) {
