020b93bdc39b40f35c7781655e7c2afc8c1a6c5f
kbirk <birk.kevin@gmail.com>
2015-10-23 15:13:55 -0400

    Fixed missing slider labels.

--- public/javascripts/ui/slider.js
+++ public/javascripts/ui/slider.js
@@ -36,12 +36,13 @@ var Slider = function(spec) {
     spec.max = spec.max !== undefined ? spec.max : 1.0;
     spec.step = spec.step !== undefined ? spec.step : 0.1;
     spec.initialValue = spec.initialValue !== undefined ? spec.initialValue : 0.1;
-    this.formatter = spec.formatter || function(value) {
+    this._formatter = spec.formatter || function(value) {
         return value.toFixed(2);
     };
-    spec.intialFormattedValue = this.formatter(spec.initialValue);
+    spec.initialFormattedValue = this._formatter(spec.initialValue);
     // create container element
     this._$container = $( SliderTemplate(spec) );
+    this._$label = this._$container.find('.control-value-label');
     // create slider and attach callbacks
     this._$slider = this._$container.find('.slider');
     this._$slider.slider({ tooltip: 'hide' });
@@ -58,7 +59,7 @@ var Slider = function(spec) {
         this._$slider.on('slide', spec.slide);
     }
     this._$slider.on('change', function( event ) {
-        self._$container.find('.control-value-label').text( self.formatter(event.value.newValue) );
+        self._$label.text( self._formatter(event.value.newValue) );
     });
 };
 
