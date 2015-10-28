2603dc1fb4743815275067b449170a4791eb3bb4
kbirk <birk.kevin@gmail.com>
2015-10-27 21:20:21 -0400

    Removed old serve-favicon dependency. Fixed typo in Dockerfiles.

--- app.js
+++ app.js
@@ -49,7 +49,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
