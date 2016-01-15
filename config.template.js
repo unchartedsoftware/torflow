1c455b3a34fda965ffa135f28e0646fd1caa8d88
cdickson <cdickson@oculusinfo.com>
2016-01-15 13:26:50 -0500

    Update dockerbuild to use environment for DB credentials.

--- config.template.js
+++ config.template.js
@@ -1,9 +1,9 @@
 var config = {
     db : {
-        host     : 'localhost',
-        user     : 'root',
-        password : 'admin',
-        database : 'torflow'
+        host     : process.env.DB_HOST || 'localhost',
+        user     : process.env.DB_USER || 'root',
+        password : process.env.DB_PASSWORD || 'admin',
+        database : process.env.DB_DATABASE || 'torflow'
     }
 };
 
